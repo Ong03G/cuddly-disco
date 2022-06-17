c090d77a682856c8c32d32aaa073f25b7ff6e5b3
Ong03G <paolo.ong@obf.ateneo.edu>
2022-06-17 17:06:44 +0800

    haha get it because it's not moving so it must mean it's parked hahaha please laugh

--- helloworld.R
+++ helloworld.R
@@ -1,3 +1,3 @@
 print("Hello World")
 print("My name is Paolo. I'm 19 years old.") 
-print("What do you call a car that's not moving? Parked.")      
+print("What do you call a car that's not moving? Parked.")    
